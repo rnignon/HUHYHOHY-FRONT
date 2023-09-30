07d5702c575a011ef9ba73cafa93cd3ec7be2187
rnignon <rnignon@naver.com>
2023-10-01 04:47:41 +0900

    Feat: Routing, Store initialize

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Outlet, Route, Routes } from "react-router-dom";
+
+import Header from "./components/Layout/Header";
+import Footer from "./components/Layout/Footer";
+import MainPage from "./pages/MainPage";
+import CrewsPage from "./pages/CrewsPage";
+import CrewDetailPage from "./pages/CrewDetailPage";
+import CrewFormPage from "./pages/CrewFormPage";
+import JoinPage from "./pages/JoinPage";
+import LoginPage from "./pages/LoginPage";
+import MyPage from "./pages/MyPage";
+
+const Layout = () => {
+  return (
+    <div>
+      <Header />
+      <Outlet />
+      <Footer />
+    </div>
+  );
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<MainPage />} />
+          <Route path="crews" element={<CrewsPage />} />
+          <Route path="crew/:crewId" element={<CrewDetailPage />} />
+          <Route path="crew/form" element={<CrewFormPage />} />
+          <Route path="join" element={<JoinPage />} />
+          <Route path="login" element={<LoginPage />} />
+          <Route path="mypage" element={<MyPage />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
